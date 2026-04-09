e58768a6c385b3c6f9b96dfa8666c02b73c2eb13
Сallme <raventeamdek@gmail.com>
2026-04-09 10:03:19 +0700

    fix merge conflicts

--- generate_id.py
+++ generate_id.py
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-import random
+﻿import random
+
 
 def generate_id():
     chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    generate_id = ''
+    generated_id = ''
     for _ in range(10):
-        generate_id += random.choice(chars)
-    return generate_id
-=======
-import random
-
-def generate_id():
-    chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    generate_id = ''
-    for _ in range(10):
-        generate_id += random.choice(chars)
-    return generate_id
->>>>>>> 4a92891bf11d83a0a39d01b4d6932b7d7751ea52
+        generated_id += random.choice(chars)
+    return generated_id
--- lists_bot.py
+++ lists_bot.py
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-city_list = ['💠 Паттайя']
+﻿city_list = ['💠 Паттайя']
 
-
-
-staff_list = ["🌸 Next Level 1 г — 45 ฿",
-"🌸 Next Level 5 г — 40 ฿ за г",
-"🌸 Next Level 10 г — 35 ฿ за г",
-=======
-city_list = ['💠 Паттайя']
-
-
-
-staff_list = ["🌸 Next Level 1 г — 45 ฿",
-"🌸 Next Level 5 г — 40 ฿ за г",
-"🌸 Next Level 10 г — 35 ฿ за г",
->>>>>>> 4a92891bf11d83a0a39d01b4d6932b7d7751ea52
-"🎁 Пробник 0.5 г — бесплатно (за отзыв)"]
+staff_list = [
+    '🌸 Next Level 1 г — 45 ฿',
+    '🌸 Next Level 5 г — 40 ฿ за г',
+    '🌸 Next Level 10 г — 35 ฿ за г',
+    '🎁 Пробник 0.5 г — бесплатно (за отзыв)',
+]
