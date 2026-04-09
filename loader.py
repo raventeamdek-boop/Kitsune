d637528f75131364a6f599c7967ab2240f53285c
raventeamdek-boop <raventeamdek@gmail.com>
2026-04-09 10:12:32 +0700

    Update loader.py

--- loader.py
+++ loader.py
@@ -13,8 +13,3 @@ from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
-<<<<<<< HEAD
-dp = Dispatcher(bot, storage=storage)
-=======
-dp = Dispatcher(bot, storage=storage)
->>>>>>> ae5eb89 (fix merge conflicts)
