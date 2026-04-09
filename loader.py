8ab6d591b4858735b35ca01dfd89edd1c3e89ec9
raventeamdek-boop <raventeamdek@gmail.com>
2026-04-09 09:59:14 +0700

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
@@ -7,7 +6,6 @@ from data import config
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-=======
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
@@ -16,4 +14,3 @@ from data import config
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
->>>>>>> 4a92891bf11d83a0a39d01b4d6932b7d7751ea52
